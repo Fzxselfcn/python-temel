bd7a1ce7a12b02ab98fec8b2c44d91f0e3287271
Furkan Çetin NAMLI <71931667+Fzxselfcn@users.noreply.github.com>
2022-12-05 11:47:17 +0300

    Update reverse.py

--- reverse.py
+++ reverse.py
@@ -1,6 +1,6 @@
-input = [[1, 2], [3, 4], [5, 6, 7]]
-input.reverse()
-for i in range(len(input)):
+input = [[1, 2], [3, 4], [5, 6, 7]] # bize input olarak verilen liste
+input.reverse() #listeye fonksiyonu uyguluyoruz
+for i in range(len(input)): #Her eleman için, alt listeyi tersine döndüren döngü
     (input[i])=(input[i])[::-1]
 
-print(input)
+print(input) #listeyi ekrana yazdırıyoruz
