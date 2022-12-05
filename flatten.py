5c1f9c5545d83e8531bdb5922c431dbde00cbede
Furkan Çetin NAMLI <71931667+Fzxselfcn@users.noreply.github.com>
2022-12-05 11:34:17 +0300

    Update flatten.py

--- flatten.py
+++ flatten.py
@@ -1,11 +1,11 @@
-liste = [[1,'a',['cat'],2],[[[3]],'dog'],4,5] // bize input olarak verilen liste
-list_flat = [] // düzleştirilen elemanları koyacağımız listeyi oluşturuyoruz
-def flatten(n): // düzleştirme fonksiyonunu yazıyoruz
+liste = [[1,'a',['cat'],2],[[[3]],'dog'],4,5] # bize input olarak verilen liste
+list_flat = [] # düzleştirilen elemanları koyacağımız listeyi oluşturuyoruz
+def flatten(n): # düzleştirme fonksiyonunu yazıyoruz
     for i in n :
         if isinstance(i,list):
             flatten(i)
         else:
             list_flat.append(i)
 
-flatten(liste) // fonksiyonumuzu listeye uyguluyoruz
-print(list_flat) // yeni listeyi ekrana yazdırıyoruz
+flatten(liste) # fonksiyonumuzu listeye uyguluyoruz
+print(list_flat) # yeni listeyi ekrana yazdırıyoruz
